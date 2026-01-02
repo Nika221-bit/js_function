8817a0751e5b51fe96b6fb640a029a31afddb6c0
NikolozLekishvili <K0K0L0z1922@gmail.com>
2026-01-03 01:21:26 +0400

    1

---
+++ main.js
@@ -0,0 +1,15 @@
+// happy new year  
+
+function happyNewYear(finalusername,age) {
+   finalusername =  finalusername.slice(0, -1);
+    console.log(`Happy New Year ${finalusername} ! you are ${age} years old`);
+    console.log(`Happy New Year ${finalusername} ! you are ${age} years old`);
+    console.log(`Happy New Year ${finalusername} ! you are ${age} years old`);
+    console.log(`Happy New Year ${finalusername} ! you are ${age} years old`);
+    console.log(`Happy New Year ${finalusername} ! you are ${age} years old`);
+    
+} 
+
+happyNewYear(prompt("Enter your name:"), prompt("Enter your age:"));
+
+
